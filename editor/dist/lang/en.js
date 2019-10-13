c464d3d460fc96a30b5d310114b831be514a3dc3
missra-kit <missra-kit@outlook.com>
2019-10-14 01:14:40 +0800

    update

---
+++ editor/dist/lang/en.js
@@ -0,0 +1,135 @@
+/*
+ * keditor.js
+ * Copyright Kothing
+ * MIT license.
+ */
+'use strict';
+
+(function (global, factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        module.exports = global.document ?
+            factory(global, true) :
+            function (w) {
+                if (!w.document) {
+                    throw new Error('KEDITOR_LANG a window with a document');
+                }
+                return factory(w);
+            };
+    } else {
+        factory(global);
+    }
+}(typeof window !== 'undefined' ? window : this, function (window, noGlobal) {
+    const lang = {
+        toolbar: {
+            default: 'Default',
+            save: 'Save',
+            font: 'Font',
+            formats: 'Formats',
+            fontSize: 'Size',
+            bold: 'Bold',
+            underline: 'Underline',
+            italic: 'Italic',
+            strike: 'Strike',
+            subscript: 'Subscript',
+            superscript: 'Superscript',
+            removeFormat: 'Remove Format',
+            fontColor: 'Font Color',
+            hiliteColor: 'Highlight Color',
+            indent: 'Indent',
+            outdent: 'Outdent',
+            align: 'Align',
+            alignLeft: 'Align left',
+            alignRight: 'Align right',
+            alignCenter: 'Align center',
+            alignJustify: 'Align justify',
+            list: 'list',
+            orderList: 'Ordered list',
+            unorderList: 'Unordered list',
+            horizontalRule: 'horizontal line',
+            hr_solid: 'solid',
+            hr_dotted: 'dotted',
+            hr_dashed: 'dashed',
+            table: 'Table',
+            link: 'Link',
+            image: 'Image',
+            video: 'Video',
+            fullScreen: 'Full screen',
+            showBlocks: 'Show blocks',
+            codeView: 'Code view',
+            undo: 'Undo',
+            redo: 'Redo',
+            preview: 'Preview',
+            print: 'print',
+            tag_p: 'Paragraph',
+            tag_div: 'Normal (DIV)',
+            tag_h: 'Header',
+            tag_blockquote: 'Quote',
+            tag_pre: 'Code',
+            template: 'Template'
+        },
+        dialogBox: {
+            linkBox: {
+                title: 'Insert Link',
+                url: 'URL to link',
+                text: 'Text to display',
+                newWindowCheck: 'Open in new window'
+            },
+            imageBox: {
+                title: 'Insert image',
+                file: 'Select from files',
+                url: 'Image URL',
+                altText: 'Alternative text'
+            },
+            videoBox: {
+                title: 'Insert Video',
+                url: 'Media embed URL, YouTube'
+            },
+            caption: 'Insert description',
+            close: 'Close',
+            submitButton: 'Submit',
+            revertButton: 'Revert',
+            proportion: 'constrain proportions',
+            width: 'Width',
+            height: 'Height',
+            basic: 'Basic',
+            left: 'Left',
+            right: 'Right',
+            center: 'Center'
+        },
+        controller: {
+            edit: 'Edit',
+            remove: 'Remove',
+            insertRowAbove: 'Insert row above',
+            insertRowBelow: 'Insert row below',
+            deleteRow: 'Delete row',
+            insertColumnBefore: 'Insert column before',
+            insertColumnAfter: 'Insert column after',
+            deleteColumn: 'Delete column',
+            resize100: 'Resize 100%',
+            resize75: 'Resize 75%',
+            resize50: 'Resize 50%',
+            resize25: 'Resize 25%',
+            mirrorHorizontal: 'Mirror, Horizontal',
+            mirrorVertical: 'Mirror, Vertical',
+            rotateLeft: 'Rotate left',
+            rotateRight: 'Rotate right',
+            maxSize: 'Max size',
+            minSize: 'Min size',
+            tableHeader: 'Table header',
+            mergeCells: 'Merge cells',
+            splitCells: 'Split Cells',
+            HorizontalSplit: 'Horizontal split',
+            VerticalSplit: 'Vertical split'
+        }
+    };
+
+    if (typeof noGlobal === typeof undefined) {
+        if (!window.KEDITOR_LANG) {
+            window.KEDITOR_LANG = {};
+        }
+
+        window.KEDITOR_LANG.en = lang;
+    }
+
+    return lang;
+}));
---
+++ editor/dist/lang/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import en from './en';
+import zh_cn from './zh_cn';
+
+export default { en, zh_cn };
